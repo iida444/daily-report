2dca85f20f2d3bf3f868255b5884ceb0767986cd
hiroyuki iida <iida444.444@gmail.com>
2026-09-13 19:16:13 +0900

    no message

--- my-modules/submit-data.js
+++ my-modules/submit-data.js
@@ -1,5 +1,5 @@
 // Cloud Firestoreにデータを送信する
-export const submitData = async (e) => {
+export const submitData = async (e, addDoc, collection, db) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
